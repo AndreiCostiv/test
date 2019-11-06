d430cf29575c93829d13323f40f042f576971b76
Andrii <andriikostiv@gmail.com>
2019-11-06 18:33:12 +0200

    Updates

---
+++ precache-manifest.9017ccbb51cbe1e6fe49c3a8f6e6cde5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3ad01e1d4d118c740185a808df7465",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f8de129c73074c7dc407",
+    "url": "/test/static/css/main.0a6a37f2.chunk.css"
+  },
+  {
+    "revision": "23c9002ce05ef8a4ca62",
+    "url": "/test/static/js/2.50059ec0.chunk.js"
+  },
+  {
+    "revision": "f8de129c73074c7dc407",
+    "url": "/test/static/js/main.c4faf2a6.chunk.js"
+  },
+  {
+    "revision": "d4478ee8f543320a5791",
+    "url": "/test/static/js/runtime-main.b6591697.js"
+  },
+  {
+    "revision": "7c18188784f21915f42a5b3bc9d91e20",
+    "url": "/test/static/media/Roboto-Bold.7c181887.ttf"
+  },
+  {
+    "revision": "c6cdfded4630ba6d9a2dceb70aa4fe0f",
+    "url": "/test/static/media/Roboto-Light.c6cdfded.ttf"
+  },
+  {
+    "revision": "18d44f79b3979ec168862093208c6d7d",
+    "url": "/test/static/media/Roboto-Regular.18d44f79.ttf"
+  }
+]);
